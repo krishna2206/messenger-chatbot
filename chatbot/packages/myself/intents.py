8d0477f30c617aabaa96f77a5b55ac57db37dd47
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-04-25 19:03:02 +0300

    removed myself package

--- chatbot/packages/myself/intents.py
+++
@@ -1,9 +0,0 @@
-from chatbot.packages.myself.myself import hello
-
-INTENTS = [
-    {
-        "type": "fallback",
-        "name": hello.__name__,
-        "action": hello
-    }
-]
--- chatbot/packages/myself/myself.py
+++
@@ -1,5 +0,0 @@
-from chatbot.messenger import SEND_API
-
-
-def hello(recipient_id):
-    SEND_API.send_text_message("Hello World !", recipient_id)
